559acf37ee085b62fbfaaa20589872b211e5c53a
iwangy <35878784+iwangy@users.noreply.github.com>
2022-05-24 19:00:47 -0700

    added getData method

--- condensation/ViewController.swift
+++ condensation/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var urlString = UserDefaults.standard.string(forKey: "quizquestionsurl") ?? "https://tednewardsandbox.site44.com/questions.json"
+    var urlString = "https://www.cheapshark.com/api/1.0/deals"
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,62 +17,31 @@ class ViewController: UIViewController {
     }
 
     fileprivate func getData() {
-        guard let url = URL.init(string: urlString) else {
-            return
-        }
-        let session = URLSession.shared.dataTask(with: url) {data, response, error in
-            if response != nil {
-                if (response! as! HTTPURLResponse).statusCode != 200 {
-                    let archiveURL = URL(fileURLWithPath: NSHomeDirectory() + "/Documents/scores.archive")
-                    let readScores = NSArray(contentsOf: archiveURL)
-                    TableDataAndDelegate.data = readScores as! [Subject]
-                } else {
-                    do {
-                        TableDataAndDelegate.data = []
-                        let questions =  try JSONSerialization.jsonObject(with: data!) as! NSArray
-                        DispatchQueue.main.async {
-                            for i in 0..<questions.count {
-                                let object = questions[i] as! NSDictionary
-                                let objectQuestions = object["questions"]! as! NSArray
-                                var QuestionArray : [Question] = []
-                                for i in 0..<objectQuestions.count {
-                                    let oneQuestion = objectQuestions[i] as! NSDictionary
-                                    QuestionArray.append(
-                                        Question(
-                                            text: oneQuestion["text"] as! String,
-                                            answer: oneQuestion["answer"] as! String,
-                                            answers: oneQuestion["answers"] as! [String]
-                                        )
-                                    )
-                                }
-                                TableDataAndDelegate.data.append(Subject(
-                                    subj: object["title"]! as! String,
-                                    desc: object["desc"]! as! String,
-                                    question: QuestionArray
-                                ))
+            guard let url = URL.init(string: urlString) else {
+                return
+            }
+            let session = URLSession.shared.dataTask(with: url) {data, response, error in
+                if response != nil {
+                    if (response! as! HTTPURLResponse).statusCode != 200 {
+                        // if response is not 200
+                        print("im sorry")
+                    } else {
+                        do {
+                            let questions =  try JSONSerialization.jsonObject(with: data!) as! NSArray
+                            print(questions)
+//                            DispatchQueue.main.async {
+//                                // make changes to UI
+//                            }
+                        } catch {
+                            DispatchQueue.main.async {
+                                print("something went wrong")
                             }
-                            self.SubjectsTableView.reloadData()
-                            print(TableDataAndDelegate.data)
-                            print(self.urlString)
-                            
-                            // UNSURE IF DATA ACTUALLY SAVES
-                            let archivePath = NSHomeDirectory() + "/Documents/quizzes.archive"
-                            print(archivePath)
-                            let quizArchive = TableDataAndDelegate.data as NSArray
-                            print(quizArchive)
-                            quizArchive.write(toFile: archivePath, atomically: true)
-                        }
-                    } catch {
-                        DispatchQueue.main.async {
-                            print("something went wrong")
-                            self.showError()
                         }
                     }
                 }
             }
+            session.resume()
         }
-        session.resume()
-    }
     
 
 }
